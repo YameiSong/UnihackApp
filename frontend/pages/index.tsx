fc558060611bc94d06d92a33d91926510cf03886
ChrisLrz <liurunze2022@gmail.com>
2024-03-01 15:59:44 +1100

    frontend webpages

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Layout from '../app/layout';
+import Homepages from './homepages';
+const Page = () => {
+  return (
+    <div>
+      <Layout />
+      <Homepages />
+      <main>
+
+      </main>
+    </div>
+  );
+};
+
+export default Page;
