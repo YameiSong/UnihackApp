ab382b25bf0538295c4755564bc9b970586eb43a
YameiSong <ymsongx@163.com>
2024-03-02 16:22:16 +1100

    create serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,22 @@
 from rest_framework.serializers import ModelSerializer, ALL_FIELDS
-from .models import Tag
+from .models import Tag, TravelPlan, Trip, User
 
 class TagSerializer(ModelSerializer):
     class Meta:
         model = Tag
+        fields = ALL_FIELDS
+
+class TravelPlanSerializer(ModelSerializer):
+    class Meta:
+        model = TravelPlan
+        fields = ALL_FIELDS
+
+class TripPlanSerializer(ModelSerializer):
+    class Meta:
+        model = Trip
+        fields = ALL_FIELDS
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = Trip
         fields = ALL_FIELDS
