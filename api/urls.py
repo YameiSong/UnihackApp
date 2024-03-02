9d258eb916a60bfc434680115e252e6c5c0021b1
YameiSong <ymsongx@163.com>
2024-03-02 15:47:04 +1100

    url and view names

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.getRoutes, name='routes'),
     path('api/tag', views.handleTag, name="handle tag"),
+    path('api/travelplan', views.handleTravelPlan, name="handle travel plan"),
+    path('api/trip', views.handleTrip, name="handle trip"),
+    path('api/login', views.handleLogin, name="handle log in"),
 ]
+    
