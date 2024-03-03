39a1b6eda6b841af1634b477387d250c2b5a4276
danielhong <dan.hong378@gmail.com>
2024-03-03 14:42:49 +1100

    added interface to notifications

--- frontend/components/notification/Notification.tsx
+++ frontend/components/notification/Notification.tsx
@@ -16,12 +16,19 @@ import { Input } from "../ui/input";
 import axiosInstance from "@/lib/axiosConfig";
 import axios from "axios";
 
+interface Notification {
+  departure_address: string,
+  arrival_address: string,
+  leave_before: string,
+  name : string | null
+}
+
 const NotificationComponent = () => {
-  const [notifications, setNotifications] = useState<string>({} as any);
+  const [notification, setNotifications] = useState<Notification | null>(null);
   const notificationRef = useRef<HTMLDivElement | null>(null);
   const detailsRef = useRef<HTMLDivElement | null>(null);
   const contactRef = useRef<HTMLInputElement | null>(null);
-  const dummyData = { destination: "Redfern", departure: "Central", departBefore: "10:00", passenger: "John Doe", rejected: false };
+  
   let notificationIsNotLoaded = useRef(false);
 
 
@@ -35,7 +42,7 @@ const NotificationComponent = () => {
           const response = await axiosInstance.get("login?user_id=1")
           console.log(response.data)
         }catch(error){
-          console.log(error);
+          console.log(error); 
         }
         setNotifications(response.data.rideSharing)
        
@@ -74,14 +81,14 @@ const NotificationComponent = () => {
   }
 
   const requestNotificationPermission = useCallback(() => {
-    // Requests permission for browser notifications.
+    // Requests permission for browser notification.
     if ('Notification' in window) {
       Notification.requestPermission()
     }
   }, [])
 
   useEffect(() => {
-    // Manages permissions for browser notifications (which is required)
+    // Manages permissions for browser notification (which is required)
     if ('Notification' in window) {
       requestNotificationPermission();
     }
@@ -89,7 +96,7 @@ const NotificationComponent = () => {
 
   useEffect(() => {
     // Sends a notification - the type of notification depends on the visibility state of the document
-    if (Object.keys(notifications).length === 0) { return; }
+    if (notification== null) { return; }
    
 
     if (document.hasFocus() && notificationRef.current) {
@@ -100,7 +107,7 @@ const NotificationComponent = () => {
       notificationIsNotLoaded.current = true;
     }
 
-  }, [notifications]);
+  }, [notification]);
 
   const acceptRideShare = () => {
     if (detailsRef.current) { detailsRef.current.click() }
@@ -114,9 +121,9 @@ const NotificationComponent = () => {
   const confirmRideShare = () => {
     const ret = {
       user_id: 1, // should be updated for the current user
-      departure_address: notifications.departure_address,
-      arrival_address: notifications.arrival_address,
-      leave_before: notifications.leave_before,
+      departure_address: notification.departure_address,
+      arrival_address: notification.arrival_address,
+      leave_before: notification.leave_before,
       contact: contactRef.current.value
     };
     
@@ -145,33 +152,35 @@ const NotificationComponent = () => {
   }
 
   return (
+    
+    <div>
+  {/* Note that this button is for TESTING PURPOSES ONLY. TODO: Remove this button after testing */}
+  <button className="p-4 bg-red-400 rounded-full text-white" onClick={handleAddNotification}>
+    (TESTING) - Manually trigger a new notification
+  </button>
+  {/* PLEASE REMOVE ABOVE AFTER TESTING */}
+  
+  {notification && (
     <>
-      {
-        // Note that this button is for TESTING PURPOSES ONLY TODO: remove this button
-      }
-      <button className="p-4 bg-red-400 rounded-full text-white" onClick={handleAddNotification}>
-        (TESTING) - manually trigger a new notification
-      </button>
-      {
-        // PLEASE REMOVE ABOVE AFTER TESTING
-      }
       <Drawer>
         <DrawerTrigger asChild>
           <div ref={notificationRef}></div>
         </DrawerTrigger>
         <DrawerContent>
           <DrawerHeader>
-            <DrawerTitle><p className="text-center">(HARDCODED NAME) wants to ride share from {notifications.departure_address} to {notifications.arrival_address}, leaving before {notifications.leave_before}</p></DrawerTitle>
-            <DrawerDescription><p className="text-center">This action cannot be undone.</p></DrawerDescription>
+            <DrawerTitle>
+              <p className="text-center">{notification&& notification.name} wants to ride share from {notification.departure_address} to {notification.arrival_address}, leaving before {notification.leave_before}</p>
+            </DrawerTitle>
+            <DrawerDescription>
+              <p className="text-center">This action cannot be undone.</p>
+            </DrawerDescription>
           </DrawerHeader>
           <DrawerFooter>
-
             <DrawerClose>
               <div className="grid grid-cols-2 gap-x-12">
                 <Button onClick={rejectRideShare} variant="outline">Reject</Button>
                 <Button onClick={acceptRideShare}>Join</Button>
               </div>
-
             </DrawerClose>
           </DrawerFooter>
         </DrawerContent>
@@ -183,28 +192,27 @@ const NotificationComponent = () => {
         </DrawerTrigger>
         <DrawerContent>
           <DrawerHeader>
-            <DrawerTitle><div className="grid md:grid-cols-2 "><p className="text-center px-3 py-2 ">Please enter your contact information</p><Input type="text" ref={contactRef} placeholder="Please provide your preferred method of communication"></Input></div></DrawerTitle>
-
+            <DrawerTitle>
+              <div className="grid md:grid-cols-2 ">
+                <p className="text-center px-3 py-2 ">Please enter your contact information</p>
+                <Input type="text" ref={contactRef} placeholder="Please provide your preferred method of communication" />
+              </div>
+            </DrawerTitle>
           </DrawerHeader>
-
-
           <DrawerFooter>
             <DrawerClose>
               <div className="grid sm:grid-cols-2 sm:grid-rows-1 gap-x-12">
                 <Button className="sm:col-start-2 sm:row-start-1" onClick={confirmRideShare}>Submit</Button>
                 <Button className="sm:col-start-1 sm:row-start-" onClick={rejectRideShare} variant="outline">Reject</Button>
-
               </div>
-
             </DrawerClose>
-
           </DrawerFooter>
-
         </DrawerContent>
       </Drawer>
-
-
     </>
+  )}
+</div>
+
   )
 };
 export default NotificationComponent;
